03359c60d3cad4dcc60ffbf1d7e158fe20633b60
Shaxa8000 <shaxboz.yaxyoyev@bk.ru>
2021-10-22 13:01:52 +0500

    added new exercises

--- JS1/script.js
+++ JS1/script.js
@@ -1,22 +1,144 @@
 // 1-masala
 
-var a = 4;
-var p = a * 4;
-console.log(p);
+// var a = 4;
+// var p = a * 4;
+// console.log(p);
 
-// 2-masala
+// // // 2-masala
 
-var a = 3;
-var s = a * a;
-console.log(s);
+// // var a = 3;
+// // var s = a * a;
+// // console.log(s);
 
-// 3-masala
+// // // 3-masala
 
-var a = 5;
-var b = 7;
-var s = a * b;
-var p = 2 * (a + b);
-console.log(p);
+// // var a = 5;
+// // var b = 7;
+// // var s = a * b;
+// // var p = 2 * (a + b);
+// // console.log(p);
+
+// // 4-masala
+
+// // var d = 15;
+// // var p = 3.14;
+// // var l = p * d;
+// // console.log(l);
+
+// // 5-masala
+
+// var a = 10;
+// var v = a ** 3;
+// var s = 6 * (a ** 2);
+// console.log(s);
+
+// 6-masala
+
+// var a = 5;
+// var b = 8;
+// var c = 10;
+// var v = a * b * c;
+// var s = 2 * (a * b + b * c + a * c);
+// console.log(v);
+// console.log(s);
+
+// 7-masala
+
+// var r = 5;
+// var p = 3.14;
+// var l = 2 * p * r;
+// var s = p * r ** 2;
+// console.log(s);
+
+// 8-masala 
+
+// var a = 10;
+// var b = 20;
+// var c = (a + b) / 2;
+// console.log(c);
+
+// 9-masala
+
+
+
+// 10-masala
+
+// var a = 2;
+// var b = 3;
+// var y = a + b;
+// var c1 = a ** 2;
+// var c2 = b ** 2;
+// console.log(y, c1, c2);
+
+
+// 13-masala
+
+// var r1 = 5;
+// var r2 = 3;
+// var p = 3.14;
+// var s1 = p * r1;
+// var s2 = p * r2;
+// var s3 = p * (r1 - r2);
+// console.log(s1, s2, s3);
+
+
+// 14 -masala 
+// var d = 20;
+// var r = d / 2;
+// var p = 3.14;
+// var l = 2 * p * r;
+// var s = p * r * (r ** 2);
+// console.log(l, s);
+
+// 17-masala
+
+// var a = 10;
+// var b = 20;
+// var c = 30;
+// var d = b - a;
+// var e = c - b;
+// var f = d + e;
+// console.log(d, e, f);
+
+// 18-masala
+
+// var a = 10;
+// var c = 30;
+// var b = 40;
+// var ac = c - a;
+// var bc = b - c;
+// console.log(ac * bc);
+
+// 20-masala
+
+
+
+
+
+// 22- masala
+// var a = 35;
+// var b = 45;
+// var c = a;
+// a = b;
+// b = c;
+// console.log(a, b);
+
+// 23-masala
+// var a = 40;
+// var b = 30;
+// var c = 20;
+// var d = a;
+// a = b;
+// b = c;
+// c = a;
+// console.log(a, b, c);
+
+
+
+
+
+
+
 
 
 
