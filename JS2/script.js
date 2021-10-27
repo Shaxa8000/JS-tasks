6f1d76194be5923a9a44b1d99c4ce9cde42b2beb
Shaxa8000 <shaxboz.yaxyoyev@bk.ru>
2021-10-27 13:03:19 +0500

    added new task

--- JS2/script.js
+++ JS2/script.js
@@ -1,11 +1,90 @@
-// 1-masala
 
-// var a = 10;
-// var b = 20;
-// var c = 30;
-// var d = a;
-// a = c;
-// c = b;
-// b = d;
+//  title = (string, times) => {
+//     if (times > 0)
+//         return string.repeat(times);
+//     else
+//          return "";
+// }
 
-// console.log(a, b, c);
+// var text = string.repeat(5);
+
+// // console.log(title('Welcome Webbrain ', 5));
+
+//  second way
+
+
+// function myFunc(string) {
+//     console.log(string.repeat(5))
+// }
+
+// myFunc("Welcome webbrain ");
+
+
+
+
+/*************** Task №2 ************** */
+// Function for Method pow
+
+// function pow(a, b) {
+//     return a ** b;
+// }
+
+// console.log(pow(4, 2));
+
+// Function for Method sqrt
+
+// function sqrt(a) {
+//     return a ** 0.5;
+// }
+
+// console.log(sqrt(81));
+
+// Function for Method cbrt
+
+// function cbrt(a) {
+//   return (a ** (1/3));
+// }
+
+//  console.log(cbrt(27));
+
+// Function for Method abs
+
+// function abs(a) {
+// return (a** 2) ** 0.5 
+// }
+// console.log(abs(-12.5));
+
+// Function for Method round
+
+// function round(a) {
+//    return (a).toFixed(0);
+
+// }
+// console.log(round(1.5))
+
+// Function for Method floor 
+
+// function floor(a) {
+//     return parseInt(a);
+// }
+// console.log(floor(8.9))
+
+// Function for Method floor2 
+
+// function floor(a) {
+//   return (a - 0.5).toFixed(0);
+// }
+// console.log(floor(6.001));
+
+
+
+// Function for Method ceil
+
+// function ceil(a) {
+//     return (a + 0.5).toFixed(0);
+// }
+// console.log(ceil(1.001));
+
+
+
+
