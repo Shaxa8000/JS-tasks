72b375f7785600370e41ebeba0e5e591a72dc82b
Shaxa8000 <shaxboz.yaxyoyev@bk.ru>
2021-10-30 13:11:10 +0500

    added new file

---
+++ JS4/script.js
@@ -0,0 +1,34 @@
+// Removing leading zeros
+
+// function myFunc() {
+//     const numStr = '123.000';
+//     var number = Number(numStr);
+//     console.log(number);
+// }
+
+// myFunc();
+
+// 2
+
+
+// function myFunc() {
+//     const numStr = "00402";
+//     var number = Number(numStr);
+//     console.log(number);
+// }
+
+// myFunc();
+
+
+//3
+
+// function myFunc() {
+//     const numStr = "03.1400";
+//     var number = Number(numStr);
+//     console.log(number);
+// }
+
+// myFunc();
+
+
+
