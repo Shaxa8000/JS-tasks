da2e3de051070609eed188600af592d1f4f10585
Shaxa8000 <shaxboz.yaxyoyev@bk.ru>
2021-10-31 17:09:53 +0500

    did some challenges

--- JS4/script.js
+++ JS4/script.js
@@ -31,4 +31,65 @@
 // myFunc();
 
 
+// 2-TASK BY USING new Set();
+
+// function myArr(arr) {
+//     return new Set(arr).size == 1;
+//     if (arr.size == 1) {
+//         console.log(true)
+//     } else {
+//         console.log(false)
+//     }
+// }
+
+// console.log(myArr(["a", "a", "a", "a"]));
+// console.log(myArr(['a', 'a', 'b', 'a']));
+
+
+//  BY USING FOR LOOP
+
+// function isEqual(arr) {
+//     var first = arr[0];
+//     for (let i = 1; i < arr.length; i++){
+//         if (first !== arr[i]) {
+//           return false;
+//         }
+//     }
+//     return true;
+// }
+
+// console.log(isEqual(['a', 'a', 'a', 'a']));
+// console.log(isEqual(['a', 'a', 'b', 'a']));
+
+
+// 3- TASK
+
+// reversedNum = (n) => {
+//     n = n + "";
+//     return n.split("").reverse().join("");
+
+// }
+  
+// console.log(reversedNum(123));
+
+//  4 -TASK
+
+// insertSpace = (string) => {
+//      string = string.replace(/([a-z])([A-Z])/g, '$1 $2');
+//      string = string.replace(/([A-Z])([A-Z][a-z])/g, '$1 $2');
+//      return string;
+// }
+
+// console.log(insertSpace('SheWalksToTheBeach'));
+// console.log(insertSpace('MawinTalksTooMuch'));
+// console.log(insertSpace('TheGreatestUpsetInHistory'));
+
+
+
+
+
+
+
+
+
 
