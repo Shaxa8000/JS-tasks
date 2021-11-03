78891d0b8e23cc7bd6612636747e6e4fd106db23
Shaxa8000 <shaxboz.yaxyoyev@bk.ru>
2021-11-03 13:33:39 +0500

    array

--- JS5/script.js
+++ JS5/script.js
@@ -10,3 +10,34 @@ function processUserInput(callback) {
 }
 
 processUserInput(greeting);
+
+// function str() {
+//     var a = "123";
+//     newStr = '';
+//     for (let i = a.length; i >= 0; i--) {
+//         newStr += a[i];
+//     }
+//     return newStr
+// }
+
+// console.log(str());
+
+let fruits = ['Apple', 'Banana']
+
+console.log(fruits.length)
+// 2
+
+let first = fruits[0]
+// Apple
+
+let last = fruits[fruits.length - 1]
+// Banana
+
+fruits.forEach(function(item, index, array) {
+  console.log(item, index)
+})
+// Apple 0
+// Banana 1
+
+
+
