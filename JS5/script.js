7ed674c21a782c4d0c1d3e2f720e6b5c3ff2b233
Shaxa8000 <shaxboz.yaxyoyev@bk.ru>
2021-11-04 14:17:49 +0500

    new challenge

--- JS5/script.js
+++ JS5/script.js
@@ -33,11 +33,29 @@ let first = fruits[0]
 let last = fruits[fruits.length - 1]
 // Banana
 
-fruits.forEach(function(item, index, array) {
-  console.log(item, index)
-})
+// fruits.forEach(function(item, index, array) {
+//   console.log(item, index)
+// })
 // Apple 0
 // Banana 1
+
+
+// let vegetables = ['Cabbage', 'Turnip', 'Radish', 'Carrot']
+// console.log(vegetables)
+// // ["Cabbage", "Turnip", "Radish", "Carrot"]
+
+// let pos = 1
+// let n = 2
+
+// let removedItems = vegetables.splice(pos, n)
+// // this is how to remove items, n defines the number of items to be removed,
+// // starting at the index position specified by pos and progressing toward the end of array.
+
+// console.log(vegetables)
+// // ["Cabbage", "Carrot"] (the original array is changed)
+
+// console.log(removedItems)
+// // ["Turnip", "Radish"]
 
 
 
