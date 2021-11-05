a098cb77ac02912192bf662efb59220aba146b31
Shaxa8000 <shaxboz.yaxyoyev@bk.ru>
2021-11-05 13:27:21 +0500

    hometask added

---
+++ JS6/script.js
@@ -0,0 +1,85 @@
+var fruits = ["Apple", 12, 'Banana', 'Orange', 'Mango'];
+var numbers = [10, 5, 11,'apple', 2, 3, 4, 5];
+
+
+
+// FINDING A AND B IN THE ARRAY
+
+// a
+
+// var str = fruits.toString();
+// var lengthOf = str.match(/a/gi).length;
+
+// console.log(`a = ${lengthOf}`);
+
+
+// b
+
+// var str = fruits.toString();
+// var lengthOf = str.match(/b/gi).length;
+
+// console.log(`b = ${lengthOf}`);
+
+
+
+// FINDING SUM OF numbers 
+// const sumOfArr = () => {
+//     var sum = 0;
+//     for (let i = 0; i < numbers.length; i++) {
+//         if (numbers[i] === "apple") {
+//             numbers.splice(i, 1);
+//       }
+//       sum += numbers[i];
+//     }
+//     console.log(sum);
+// }
+
+// sumOfArr();
+
+
+
+// FINDING SUM OF ODD AND EVEN NUMBERS
+// even
+
+const sumOfArr = () => {
+    var sum = 0;
+    for (let i = 0; i < numbers.length; i+=2) {
+        if (numbers[i] === "number") {
+            return numbers[i]
+      }
+      sum += numbers[i];
+    }
+    console.log(sum);
+}
+
+sumOfArr();
+
+
+// odd 
+// const num = [2, 4, 5, 7, 2, 8]
+// const sumofArr = () => {
+//     var sum = 0;
+//     for (let i = 1; i < num.length; i+=2) {
+//       sum += num[i];
+//     }
+//     console.log(sum);
+// }
+// sumofArr();
+
+
+// Finding the second highest and second lowest value
+
+//second biggest:
+// const number = [2, 15, 76, 81, 17];
+// const num = number.sort((a, b) => b - a)
+// console.log(num[1]);
+
+
+
+//second lowest
+
+// const number = [2, 15, 76, 81, 17];
+// const num = number.sort((a, b) => a - b);
+// console.log(num[1]);
+
+
