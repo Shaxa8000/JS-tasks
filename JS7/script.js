0610c21b1a85195de756d3a605acb368334eab3f
Shaxa8000 <shaxboz.yaxyoyev@bk.ru>
2021-11-08 13:18:56 +0500

    added hometask

---
+++ JS7/script.js
@@ -0,0 +1,58 @@
+const user = [
+ {
+  name: 'Eshmat',
+  year: 2011,
+  id: 1,
+  status: 'student',
+  key: 'test',
+},
+
+{
+  name: 'Eshmat akasi',
+  year: 2012,
+  id: 2,
+  status: 'programmer',
+  key: 'test',
+},
+
+{
+  name: 'Toshmat',
+  year: 2010,
+  id: 3,
+  status: 'student',
+  key: "test",
+}
+]
+
+//<<< sorting by year>>>
+
+// const userSort = user.sort(function (a, b) {
+//   if (a.year > b.year) {
+//     return 1
+//   } else {
+//     return -1
+//   }
+// })
+
+// console.log(userSort);
+
+
+// <<< sorting by name>>>
+
+// const sortName = user.filter(function (value) {
+//   if (value.name === 'Eshmat') {
+//     return value
+//   }
+// })
+
+// console.log(sortName);
+
+
+//<<< filtering by the status>>
+
+// const filterStatus = user.filter(value => value.status === 'student');
+
+// console.log(filterStatus);
+
+
+
