3c91b568d5726770f21cb0a11d80d2a706011f47
Shaxa8000 <shaxboz.yaxyoyev@bk.ru>
2021-11-09 23:27:59 +0500

    added challenge

--- JS7/script.js
+++ JS7/script.js
@@ -24,6 +24,13 @@ const user = [
 }
 ]
 
+//<< Deleting object with current id>>>
+
+
+// const onDelete = id => user.filter(value => value.id !== id);
+// console.log(onDelete(2));
+
+
 //<<< sorting by year>>>
 
 // const userSort = user.sort(function (a, b) {
@@ -41,7 +48,7 @@ const user = [
 
 // const sortName = user.filter(function (value) {
 //   if (value.name === 'Eshmat') {
-//     return value
+//     return value;
 //   }
 // })
 
@@ -53,6 +60,63 @@ const user = [
 // const filterStatus = user.filter(value => value.status === 'student');
 
 // console.log(filterStatus);
+
+
+//<<< Calling by key and name>>>    FILTER ADVANCED
+
+// const filterAdvanced = (key, value) => {
+//   const res = [];
+//   user.map(val => {
+//     if (val[key].includes(value)) {
+//       for (const kalit in val) {
+//         if (kalit === key) {
+//           res.push(val)
+//         }
+//       }
+//     }
+//   })
+//   return res;
+// };
+
+// console.log(filterAdvanced('name', "Eshmat"));
+
+
+
+
+
+// // Animal properties and method encapsulation
+// var Animal = {
+//   type: 'Invertebrates', // Default value of properties
+//   displayType: function() {  // Method which will display type of Animal
+//     console.log(this.type);
+//   }
+// };
+
+// // Create new animal type called animal1
+// var animal1 = Object.create(Animal);
+// animal1.displayType(); // Output:Invertebrates
+
+// // Create new animal type called Fishes
+// var fish = Object.create(Animal);
+// fish.type = 'Fishes';
+// fish.displayType(); // Output:Fishes
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
